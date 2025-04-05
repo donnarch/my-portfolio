7913b25caedafd627dc4330e9a71c160252219b0
doniyor <doniyor@gmail.com>
2025-04-05 15:37:45 +1200

    my portfolio

---
+++ js/data.js
@@ -0,0 +1,32 @@
+export default [
+  {
+    title: "Ansormed",
+    github: "https://github.com/donnarch/Ansormeed",
+    vercel: "https://ansormeed-k5t2.vercel.app/",
+  },
+  {
+    title: "Akademnashr",
+    github: "https://github.com/donnarch/akademnashr",
+    vercel: "https://akademnashr-bice.vercel.app/",
+  },
+  {
+    title: "Coffee street",
+    github: "https://github.com/donnarch/caffee",
+    vercel: "https://3-mu-lake.vercel.app/",
+  },
+  {
+    title: "Bem h/m 3",
+    github: "https://github.com/donnarch/bem-homework-2",
+    vercel: "https://bem-homework-2.vercel.app/",
+  },
+  {
+    title: "Bem h/m4",
+    github: "https://github.com/donnarch/bem-homework-4",
+    vercel: "https://bem-homework-4.vercel.app/",
+  },
+  {
+    title: "Bem h/m",
+    github: "https://github.com/donnarch/bem-pack",
+    vercel: "https://bem-pack.vercel.app/",
+  },
+];
